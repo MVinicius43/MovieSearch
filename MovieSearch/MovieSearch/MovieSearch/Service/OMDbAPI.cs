7de8b22bcc8b51603a56085bcc40c7bb8825e271
Marcos Vinícius Emídio Silva <mviniciusess@gmail.com>
2020-04-25 15:58:30 -0300

    First Commit

---
+++ MovieSearch/MovieSearch/MovieSearch/Service/OMDbAPI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieSearch.Service
+{
+    public class OMDbAPI
+    {
+        private string apiURL = "http://www.omdbapi.com/?i=tt3896198&apikey=255a9abf";
+
+        
+    }
+}
